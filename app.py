f47b73b7b9efa0cf775de97b1e6e608b16d2d155
Det-Mac15 <det-mac15@The-Hidden-Genius-Project-16.local>
2023-11-08 16:43:13 -0500

    first commit

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from markupsafe import escape
-from flask import Flask, abort 
+from flask import Flask
 
 app = Flask(__name__)
 
@@ -11,22 +11,7 @@ def hello():
 @app.route('/about/')
 def about():
     return '<h3>This is a Flask web application.</h3>'
-#
 
 @app.route('/capitalize/<word>/')
 def capitalize(word):
     return '<h1>{}</h1>'.format(escape(word.capitalize()))
-#
-
-@app.route('/add/<int:n1>/<int:n2>/')
-def add(n1, n2):
-    return '<h1>{}</h1>'.format(n1 + n2)
-#
-
-@app.route('/users/<int:user_id>/')
-def greet_user(user_id):
-    users = ['Bob', 'Jane', 'Adam']
-    try:
-        return '<h2>Hi {}</h2>'.format(users[user_id])
-    except IndexError:
-        abort(404)
